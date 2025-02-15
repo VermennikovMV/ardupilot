cbdd253ea43ea557cdb90a4258dcd2f6bbe420e6
VermennikovMV <minatavr65@gmail.com>
2025-02-15 13:32:19 +0300

    minor compiler fix

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -228,6 +228,7 @@ public:
     friend class ModeZigZag;
     friend class ModeAutorotate;
     friend class ModeTurtle;
+    friend class ModeUserDefined;
 
     friend class _AutoTakeoff;
 
--- ArduCopter/mode_userdefined.cpp
+++ ArduCopter/mode_userdefined.cpp
@@ -1,7 +1,7 @@
 #include "Copter.h"
 #include <AP_Mount/AP_Mount.h>
 
-#if MODE_CIRCLE_ENABLED
+#if MODE_USER_ENABLED
 
 /*
  * Init and run calls for circle flight mode
