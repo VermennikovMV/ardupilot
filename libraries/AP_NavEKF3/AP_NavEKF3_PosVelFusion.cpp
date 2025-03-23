93ad624e3635f9f52255ddddb79dd3a787c753f0
VermennikovMV <minatavr65@gmail.com>
2025-03-23 17:56:08 +0300

    minor fix

--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -198,7 +198,7 @@ bool NavEKF3_core::setLatLng(const Location &loc, float posAccuracy, uint32_t ti
         snprintf(message, sizeof(message),
             "imuSampleTime_ms: %lu, lastGpsPosPassTime_ms: %lu, PV_AidingMode: %d, validOrigin: %d",
             imuSampleTime_ms, lastGpsPosPassTime_ms, PV_AidingMode, validOrigin);
-        gcs().send_text(MAV_SEVERITY_EMERGENCY, message);
+            gcs().send_text(MAV_SEVERITY_EMERGENCY, "%s", message);
         return false;
     }
 
