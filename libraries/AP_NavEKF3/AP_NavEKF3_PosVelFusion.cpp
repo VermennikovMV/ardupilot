2bc95036b4b6594c2a10e7a2d3edb4c53a81cfc3
VermennikovMV <minatavr65@gmail.com>
2025-03-27 12:15:52 +0300

    Гипотеза3 (home_point + params)

--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -191,12 +191,6 @@ void NavEKF3_core::ResetPosition(resetDataSource posResetSource)
 bool NavEKF3_core::setLatLng(const Location &loc, float posAccuracy, uint32_t timestamp_ms)
 {
     gcs().send_text(MAV_SEVERITY_EMERGENCY, "I'm second! AP_NavEKF3_PosVelFusion.cpp");
-    readGpsData();
-    const auto &gps = dal.gps();
-    const Location &gpsloc = gps.location(selected_gps);
-    Location gpsloc_fieldelevation = gpsloc; 
-    setOrigin(gpsloc_fieldelevation);
-    setAidingMode();
     char message[100];
     if ((imuSampleTime_ms - lastGpsPosPassTime_ms) < frontend->deadReckonDeclare_ms ||
          (PV_AidingMode == AID_NONE)
