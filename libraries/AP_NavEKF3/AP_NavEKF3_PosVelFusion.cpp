1d85d22eeb14b9e7cbfe5a7ad9bff601bf148a27
VermennikovMV <minatavr65@gmail.com>
2025-03-27 20:01:38 +0300

    upgraded debug

--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -191,13 +191,21 @@ void NavEKF3_core::ResetPosition(resetDataSource posResetSource)
 bool NavEKF3_core::setLatLng(const Location &loc, float posAccuracy, uint32_t timestamp_ms)
 {
     gcs().send_text(MAV_SEVERITY_EMERGENCY, "I'm second! AP_NavEKF3_PosVelFusion.cpp");
+    char message[100];
     if ((imuSampleTime_ms - lastGpsPosPassTime_ms) < frontend->deadReckonDeclare_ms ||
-        (PV_AidingMode == AID_NONE)
-        || !validOrigin) {
-        gcs().send_text(MAV_SEVERITY_EMERGENCY, "Failing at AP_NavEKF3_PosVelFusion.cpp!!! validOrigin = false or deadReckonDeclare_ms is too small number");
+         (PV_AidingMode == AID_NONE)
+         || !validOrigin) {
+        snprintf(message, sizeof(message),
+            "imuSampleTime_ms: %lu, lastGpsPosPassTime_ms: %lu, PV_AidingMode: %d, validOrigin: %d",
+            imuSampleTime_ms, lastGpsPosPassTime_ms, PV_AidingMode, validOrigin);
+            gcs().send_text(MAV_SEVERITY_EMERGENCY, "%s", message);
         return false;
     }
-
+    snprintf(message, sizeof(message),
+    "imuSampleTime_ms: %lu, lastGpsPosPassTime_ms: %lu, PV_AidingMode: %d, validOrigin: %d",
+    imuSampleTime_ms, lastGpsPosPassTime_ms, PV_AidingMode, validOrigin);
+    gcs().send_text(MAV_SEVERITY_EMERGENCY, "%s", message);
+    
     // Store the position before the reset so that we can record the reset delta
     posResetNE.x = stateStruct.position.x;
     posResetNE.y = stateStruct.position.y;
