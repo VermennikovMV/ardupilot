c5563e7545928dd65ba88880505eb01aa9fea7f6
VermennikovMV <minatavr65@gmail.com>
2025-07-07 18:37:08 +0300

    Update mode_eight.cpp

--- ArduPlane/mode_eight.cpp
+++ ArduPlane/mode_eight.cpp
@@ -1,53 +1,18 @@
-#include "mode.h"
-#include "Plane.h"
-
-bool ModeEight::_enter()
+class ModeFigure8 : public Mode
 {
-    plane.do_loiter_at_location();
-    plane.setup_terrain_target_alt(plane.next_WP_loc);
-    plane.set_target_altitude_current();
-    plane.loiter_angle_reset();
+public:
 
-    radius_m = (fabsf(plane.aparm.loiter_radius) <= 1) ? LOITER_RADIUS_DEFAULT : fabsf(plane.aparm.loiter_radius);
+    Number mode_number() const override { return Number::FIGURE8; }
+    const char *name() const override { return "FIGURE8"; }
+    const char *name4() const override { return "FIG8"; }
 
-    cross_loc = plane.next_WP_loc;
-    origin_yaw_rad = radians(ahrs.yaw_sensor * 0.01f);
-    Vector2f ofs(radius_m, 0);
-    ofs.rotate(origin_yaw_rad + M_PI_2);
+    void update() override;
 
-    right_loc = cross_loc;
-    right_loc.offset(ofs.x, ofs.y);
+protected:
+    bool _enter() override;
 
-    left_loc = cross_loc;
-    left_loc.offset(-ofs.x, -ofs.y);
-
-    direction = 1;
-    plane.next_WP_loc = right_loc;
-    plane.next_WP_loc.loiter_ccw = 0;
-    plane.loiter.direction = direction;
-
-    return true;
-}
-
-void ModeEight::update()
-{
-    plane.calc_nav_roll();
-    plane.calc_nav_pitch();
-    plane.calc_throttle();
-
-    plane.update_loiter(0);
-
-    if (labs(plane.loiter.sum_cd) >= 18000) {
-        direction = -direction;
-
-        if (direction > 0) {
-            plane.next_WP_loc = right_loc;
-            plane.next_WP_loc.loiter_ccw = 0;
-        } else {
-            plane.next_WP_loc = left_loc;
-            plane.next_WP_loc.loiter_ccw = 1;
-        }
-        plane.loiter.direction = direction;
-        plane.loiter_angle_reset();
-    }
-}
+private:
+    int8_t direction = 1;
+    uint32_t last_switch_ms = 0;
+    static constexpr uint32_t switch_period_ms = 5000;
+};
