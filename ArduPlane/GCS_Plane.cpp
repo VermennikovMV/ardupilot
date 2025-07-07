bf6fd004217a7f54c6def6509dd8c32d1cc36a56
VermennikovMV <minatavr65@gmail.com>
2025-07-07 17:39:30 +0300

    Fix switch cases for new EIGHT mode

--- ArduPlane/GCS_Plane.cpp
+++ ArduPlane/GCS_Plane.cpp
@@ -64,6 +64,7 @@ void GCS_Plane::update_vehicle_sensor_status_flags(void)
     case Mode::Number::AVOID_ADSB:
     case Mode::Number::GUIDED:
     case Mode::Number::CIRCLE:
+    case Mode::Number::EIGHT:
     case Mode::Number::TAKEOFF:
 #if MODE_AUTOLAND_ENABLED
     case Mode::Number::AUTOLAND:
